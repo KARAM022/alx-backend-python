1ed01f55133d21ba4bc2bb22958451c4b7db3361
KARAM022 <my0fav0library@gmail.com>
2024-08-05 16:52:16 +0100

    0x01

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
-"""concurrent coroutines"""
+"""Concurrent coroutines"""
 
 import asyncio
-
+import random
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
+    
+    # Sort the list of delays using an insertion sort algorithm
+    for i in range(1, len(delays)):
+        key = delays[i]
+        j = i - 1
+        while j >= 0 and key < delays[j]:
+            delays[j + 1] = delays[j]
+            j -= 1
+        delays[j + 1] = key
 
-async def wait_n(n: int, max_delay: int) -> list:
-    """Wait for a random delay between 0 and max_delay"""
-    delays = [wait_random(max_delay) for i in range(n)]
-    return [await delay for delay in asyncio.as_completed(delays)]
+    return delays
