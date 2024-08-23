44d46d65ff819a209f46c5596120083e7b601fec
KARAM022 <my0fav0library@gmail.com>
2024-08-23 16:13:02 +0100

    fst test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,7 +5,7 @@ import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
 
-from .utils import access_nested_map
+from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
