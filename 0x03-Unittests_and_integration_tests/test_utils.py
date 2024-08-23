f439e8e9687c68cf709f1df8a47f96a118212797
KARAM022 <my0fav0library@gmail.com>
2024-08-23 16:23:24 +0100

    scnd test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,3 +19,13 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, result):
         """ Test access_nested_map """
         self.assertEqual(access_nested_map(nested_map, path), result)
+        
+    
+    @parameterized.expand([
+        ({}, ('a',), KeyError),
+        ({'a': 1}, ('a', 'b'), KeyError),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, result):
+        """ Test access_nested_map exception """
+        with self.assertRaises(result):
+            access_nested_map(nested_map, path)
